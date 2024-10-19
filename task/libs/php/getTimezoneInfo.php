854906ee96ce13d98f91a3dd7fb52ef4af2bc361
Ainotna95 <a.mcdonnell123@gmail.com>
2024-10-19 10:44:00 +0100

    Added Country Name information to index.html and successfully connected the API URL to the index.html page so that it displays the data on the page

--- task/libs/php/getTimezoneInfo.php
+++ task/libs/php/getTimezoneInfo.php
@@ -1,49 +1,62 @@
 <?php
+	//header is a function that sets the HTTP headers for the response
+	header('Content-Type: application/json; charset=UTF-8');
 
-	// remove for production
-
-	//initiating comprehensive error reporting so that the routine (a sequence of code that is intended to be used repeatedly during the execution of a programme) runs in the browser
+	//initiating comprehensive error reporting so that the routine runs in the browser
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 
 	//initiating the execution time of the routine so that it can be measured
 	$executionStartTime = microtime(true);
 
-	//concatenates the URL for the API call with the required parameters passed from the data section of the AJAX call in the script.js files
-    $url= 'http://api.geonames.org/timezoneJSON?formatted=true&lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=amcdonnell';
-	//initiates the cURL object and sets some parameters
-	$ch = curl_init();
-	//CURLOPT_SSL_VERIFYPEER is a boolean value that determines whether the cURL object should verify the peer's certificate. The peer's certificate is the certificate that the server presents to the client during the SSL handshake
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	//CURLOPT_RETURNTRANSFER is a boolean value that determines whether the cURL object should return the result of the API call
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	//CURLOPT_URL is a string that contains the URL for the API call
-	curl_setopt($ch, CURLOPT_URL,$url);
- 
-	//executes the cURL object and returns the result
-	$result=curl_exec($ch);
+	// Ensure lat and lng are set in the request
+	if (isset($_REQUEST['lat']) && isset($_REQUEST['lng'])) {
+	    $lat = $_REQUEST['lat'];
+	    $lng = $_REQUEST['lng'];
+	} else {
+	    // If lat or lng are not present, return an error response
+	    $output['status']['code'] = "400";
+	    $output['status']['name'] = "error";
+	    $output['status']['description'] = "Missing latitude or longitude";
+	    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	    echo json_encode($output);
+	    exit();
+	}
 
-	//closes the cURL object so that it can be used again
+	// Concatenates the URL for the API call with the required parameters
+    $url = 'http://api.geonames.org/timezoneJSON?formatted=true&lat=' . $lat . '&lng=' . $lng . '&username=amcdonnell&style=full';
+
+	// Initiates the cURL object and sets some parameters
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Disable SSL certificate verification
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // Return the API response as a string
+	curl_setopt($ch, CURLOPT_URL, $url);  // Set the API URL
+
+	// Executes the cURL object and returns the result
+	$result = curl_exec($ch);
+
+	// Closes the cURL object so that it can be used again
 	curl_close($ch);
 
-	//This particular API returns data as JSON and so we need to encode and decode the JSON string as an associative array because it is not in the correct format. The correct format is the associative array ("geonames": [{ "continent": EU, capital: "London", "languages": "en-GB, cy-GB, gd", etc.}]).
-	//decodes/ converts the JSON string returned by the API call and stores it in the $decode variable. Decode means to convert a string from one format to another.
-	$decode = json_decode($result,true);	
-	//stores the decoded JSON string in the $output variable
-	//$output is an associative array that contains the data returned by the API call
-	$output['status']['code'] = "200";
-	//$output['status']['name'] is a string that contains the name of the status of the API call
-	$output['status']['name'] = "ok";
-	//$output['status']['description'] is a string that contains the description of the status of the API call
-	$output['status']['description'] = "success";
-	//$output['status']['returnedIn'] is a string that contains the time taken to execute the API call
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	//the "geonames" property from the serialised JSON is stored into the "data" property of the $output variable
-	//the $decode["geonames"] in the code below returns the "geonames" property from the serialised JSON
-	$output['data'] = $decode['geonames'];
-	
-	//header is a function that sets the HTTP headers for the response
-	header('Content-Type: application/json; charset=UTF-8');
-	//echo json_encode($output) is a function that converts the $output variable to a JSON string and sends it to the client
+	// Decodes the JSON string returned by the API call and stores it in the $decode variable
+	$decode = json_decode($result, true);
+
+	// Check if the API response contains valid data
+	if (isset($decode['lat']) && isset($decode['lng'])) {
+	    // Stores the decoded JSON string in the $output variable
+	    $output['status']['code'] = "200";
+	    $output['status']['name'] = "ok";
+	    $output['status']['description'] = "success";
+	    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	    $output['data'] = $decode;
+	} else {
+	    // If the API call failed or returned an unexpected response, return an error
+	    $output['status']['code'] = "500";
+	    $output['status']['name'] = "error";
+	    $output['status']['description'] = "Failed to retrieve data from the API";
+	    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	}
+
+	// Echo json_encode($output) is a function that converts the $output variable to a JSON string and sends it to the client
 	echo json_encode($output);
 ?>
