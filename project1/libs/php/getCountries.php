190f7fdac130db9e8e06005e5501c28c849e856a
Ainotna95 <a.mcdonnell123@gmail.com>
2024-10-26 15:52:36 +0100

    Amended file format to only have one js folder

--- project1/libs/php/getCountries.php
+++ project1/libs/php/getCountries.php
@@ -56,6 +56,8 @@ if (isset($decode["features"])) {
 ?>
 =======
 header('Content-Type: application/json');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache"); // For HTTP/1.0 compatibility
 
 // Enable error reporting
 ini_set('display_errors', 'On');
