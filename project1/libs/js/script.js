b0e758875aa7f7521b5efa3fc4ea257ee5920d3a
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-03 13:49:06 +0000

    Amending how the information is added to the <option> tag

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -964,28 +964,34 @@ $(window).on('load', function () {
 // GLOBAL DECLARATIONS
 // ---------------------------------------------------------
 
+//store the leaflet map instance
 var map;
 
-// tile layers
+// store the tile layer interface to change map layers
 var layerControl;
 
+var countryData = [];
+
+//street map layer
 var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
   }
 );
 
+//satellite map layer
 var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
   }
 );
 
+//layers are stored in basemaps variable to facilitate easy switching
 var basemaps = {
   "Streets": streets,
   "Satellite": satellite
 };
 
 // buttons
-
+//fa-info icon (Font Awesome) info button in a larger size (fa-xl), which opens a modal with the ID "exampleModal" when clicked
 var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
   $("#exampleModal").modal("show");
 });
@@ -994,24 +1000,46 @@ var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
 // EVENT HANDLERS
 // ---------------------------------------------------------
 
-// initialise and add controls once DOM is ready
+// initialise map with streets as the default layer once DOM is ready
 
 $(function () {
   
   map = L.map("map", {
     layers: [streets]
-  }).setView([54.5, -4], 6);
+  });
   
   // setView is not required in your application as you will be
   // deploying map.fitBounds() on the country border polygon
 
+  //adds the layer control to the map allowing the user to toggle between streets and satellite layers
   layerControl = L.control.layers(basemaps).addTo(map);
 
+  //adds the info button to the map
   infoBtn.addTo(map);
 
+  //checks if the user has geolocation enabled
+  //if not, displays an alert
+  //otherwise, it calls the getPosition function, which gets the user's location
+  if(!navigator.geolocation) {
+    alert("Your browser does not support geolocation");
+  } else {
+    navigator.geolocation.watchPosition(getPosition);
+  }
 
+  //getPosition fetches latitude, longitude and accuracy from the position object
+  //and logs them to the console
+  function getPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    //var accuracy = position.coords.accuracy;
+    //console.log("latitude: ", lat, "longitude: ", lng, "accuracy: ", accuracy);
+    //TO AMEND: set the map view to the user's location using .fitBounds()
+    //currently centres the map on the user's location at a zoom level of 6
+    map.setView([lat, lng], 6);
 
+  }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 //the function will be called when the user's location is successfully retrieved
@@ -1192,36 +1220,47 @@ navigator.geolocation.watchPosition(success, error);
 =======
 
   // Load countries into select box on page load
+=======
+  //sends a request to getCountries.php to get a list of countries, expecting a JSON response
+>>>>>>> c011914 (Amending how the information is added to the <option> tag)
   $.ajax({
     url: "libs/php/getCountries.php",
     type: "GET",
     dataType: "json",
+    //if successful, it logs the response to the console
+    //and constructs options for a dropdown menu (countrySelect)
+    //based on the returned data
     success: function(result) {
-  console.log(result);
-  //if the request is successful
+      //console.log(result);
       if (result.status.name == "ok") {
-        //create an empty options variable
-        let options = "";
-        //for each country in the result
+        //loop through the data and create an option for each country
         result.data.forEach(country => {
-          //add an option to the options variable
-          options += `<option value="${country.id}">${country.name}</option>`;
-        });
-        //set the options variable to the select box so that it is displayed on the page
-        $("#countrySelect").html(options);
+
+          countryData.push({
+            code: country["iso_a2"],
+            name: country["name"]
+          })
+
+          //console.log(country);
+          $("<option>")
+          //adds the country name as text to the dropdown menu
+          //and country iso code as the value
+          .val(country["iso_a2"])
+          .text(country["name"])
+          //appends the country name and iso code to the select element/ dropdown menu
+          .appendTo("#countrySelect");
+        });         
       }
     },
+    //if an error occurs, logs the error information to the console
     error: function(jqXHR, textStatus, errorThrown) {
-
       console.log(`Error: ${textStatus} - ${errorThrown}`);
+      //console.log(jqXHR.responseText);
     }
-
   });
 
-  // Handle select change event
-  $("#countrySelect").on("change", function() {
-    const selectedCountryId = $(this).val();
-  });
+  //ADD FILTERING TO APPLICATION
+
 });
 
 });
