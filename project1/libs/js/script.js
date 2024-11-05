eaa33721ce5240ed55eb94b1db46fc1c7fdf863a
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-05 20:35:24 +0000

    Cleaning up code

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -110,17 +110,6 @@ $(window).on("load", function () {
         return;
       }
 
-      /*// Sanitize coordinates function
-      function sanitizeCoordinates(coordinates) {
-        return coordinates.map(coord => {
-          if (Array.isArray(coord[0])) {
-            return sanitizeCoordinates(coord); // Recursively process nested arrays
-          } else {
-            return coord.slice(0, 2); // Return [lng, lat] pairs
-          }
-        });
-      }*/
-
       // AJAX request to get country border data
       $.ajax({
         url: 'libs/php/getCountryBorders.php',
@@ -134,15 +123,10 @@ $(window).on("load", function () {
           }
           
           const borderCoordinates = response.data; // Access the coordinates
-          //console.log("border coordinates test: ", borderCoordinates[0][0]);
-          /*const sanitizedBorderCoordinates = sanitizeCoordinates(borderCoordinates);*/
-          //console.log(borderCoordinates[0][0][0]);
 
           // Determine whether the structure is MultiPolygon or Polygon
           const isMultiPolygon = Array.isArray(borderCoordinates[0][0]) && Array.isArray(borderCoordinates[0][0][0]);
           //console.log("isMultiPolygon: ", isMultiPolygon);
-
-        
 
           const geoJsonData = {
             type: "Feature",
