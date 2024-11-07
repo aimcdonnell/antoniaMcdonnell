16145a90e7b78b035a7e80ab1b0e08492195098d
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-07 17:06:29 +0000

    Adding buttons to the map

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -39,6 +39,22 @@ $(window).on("load", function () {
     $("#exampleModal").modal("show");
   });
 
+  var cloudBtn = L.easyButton("fa-cloud fa-xl", function (btn, map) {
+    $("#cloudModal").modal("show");
+  });
+
+  var currencyBtn = L.easyButton("fa-solid fa-dollar-sign fa-xl", function (btn, map) {
+    $("#currencyModal").modal("show");
+  });
+
+  var wikipediaBtn = L.easyButton("fa-brands fa-wikipedia-w fa-xl", function (btn, map) {
+    $("#wikipediaModal").modal("show");
+  });
+
+  var newspaperBtn = L.easyButton("fa-solid fa-newspaper fa-xl", function (btn, map) {
+    $("#newspaperModal").modal("show");
+  });
+
   // ---------------------------------------------------------
   // EVENT HANDLERS
   // ---------------------------------------------------------
@@ -56,8 +72,13 @@ $(window).on("load", function () {
     // Add the layer control to the map
     layerControl = L.control.layers(basemaps).addTo(map);
 
-    // Add the info button to the map
+    // Add the buttons to the map
     infoBtn.addTo(map);
+    cloudBtn.addTo(map);
+    currencyBtn.addTo(map);
+    wikipediaBtn.addTo(map);
+    newspaperBtn.addTo(map);
+    
 
     // AJAX request to get countries
     $.ajax({
