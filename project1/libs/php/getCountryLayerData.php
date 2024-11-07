329558e6c7874d49177c15304747c135dfd2cff0
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-07 19:11:21 +0000

    Added Bootstrap stylesheet to app and began implementing countryLayerData php file

---
+++ project1/libs/php/getCountryLayerData.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//initiating the execution time of the routine so that it can be measured
+$executionStartTime = microtime(true);
+
+
+
+?>
