b2529f50f19499de3e950c61198c19bc8ab22c3f
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-07 13:55:35 +0000

    Adding borders to the user's country location

--- project1/libs/php/getGeocodeData.php
+++ project1/libs/php/getGeocodeData.php
@@ -36,18 +36,31 @@ error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 
-$api_key = "0d315a3d7ebc4c95983e51902d24a8a1";
-$lat = $_REQUEST["lat"];
-$lng = $_REQUEST["lng"];
 
-$url = "https://api.opencagedata.com/geocode/v1/json?q=" . $lat . ',' . $lng . '&key=' . $apiKey;
+$apiKey = "0d315a3d7ebc4c95983e51902d24a8a1";
+
+if (isset($_REQUEST["lat"]) && isset($_REQUEST["lng"])) {
+    $lat = $_REQUEST["lat"];
+    $lng = $_REQUEST["lng"];
+} else {
+    // Handle missing parameters, e.g., return an error response or set default values
+    echo "Latitude and longitude are required.";
+    exit;
+}
+
+$url = "https://api.opencagedata.com/geocode/v1/json?q=" . $lat . "," . $lng . "&key=" . $apiKey;
 
 $ch = curl_init();
+<<<<<<< HEAD
 >>>>>>> e1239ab (Working on the user location's marker code)
+=======
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+>>>>>>> 502b667 (Adding borders to the user's country location)
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($ch);
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 curl_close($ch);
@@ -71,4 +84,23 @@ curl_close($ch);
 
 echo $result;
 >>>>>>> e1239ab (Working on the user location's marker code)
+=======
+
+curl_close($ch);
+
+$decode = json_decode($result, true);
+
+if (isset($decode["results"][0])) {
+
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["status"]["returnedIn"] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    $output["data"] = $decode["results"];
+    
+    echo json_encode($output);
+} else {
+    echo "No results found";
+}
+>>>>>>> 502b667 (Adding borders to the user's country location)
 ?>
