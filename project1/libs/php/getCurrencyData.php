032e89d8c3e569ae9ae9e86a2f2da09e808377f3
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-11 19:57:51 +0000

    Adding currency converter to app

---
+++ project1/libs/php/getCurrencyData.php
@@ -0,0 +1,39 @@
+<?php
+//Getting currency data from the Open Exchange Rates API
+
+header("Content-Type: application/json; charset=UTF-8");
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
+$apiKey = "ac1427d7ddbc49abb7374c7e40489199";
+
+$url = "https://openexchangerates.org/api/latest.json?app_id=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result, true);
+
+if (isset($decode["rates"])) {
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["status"]["returnedIn"] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    $output["data"] = $decode;
+
+    echo json_encode($output);
+} else {
+    echo "No results found";
+}
+
+?>
