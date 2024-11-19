eeb91104fb6abd13dee1a8a4117a02100a6ad8f0
Ainotna95 <a.mcdonnell123@gmail.com>
2024-11-19 11:37:34 +0000

    Removing error from code

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1928,7 +1928,7 @@ navigator.geolocation.watchPosition(success, error);
               data: { lat, lng },
               success: function (response) {
                 //console.log(response);
-                if (response.status.name === "ok" && Array.isArray(response.data) && response.data.length > 0) {
+                if (response && response.status && response.status.name === "ok" && Array.isArray(response.data) && response.data.length > 0) {
                       response.data.forEach(poi => {
                         let markerIcon;
                       if (markerIcon) {}
